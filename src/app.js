5837ec7becf340db2c6ea3115bbefeea3ba44315
mindphreaked <midhuns@qburst.com>
2017-12-27 16:21:38 +0530

    created index.html, and sample component.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-let abc = () => {
-    console.log("juuice");
-
-}
-abc();
+import Hello from './components/hello'
+import React from 'react'
+import {render} from 'react-dom'
+render(<Hello />, document.getElementById('app'));
---
+++ src/components/hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function Hello () {
+  return (
+    <div>
+      Hello React!
+    </div>
+  )
+}
+export default Hello
