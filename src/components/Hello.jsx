8d301952de5aadbc5ba31bf455c89bead7cfde09
mindphreaked <midhuns@qburst.com>
2017-12-28 15:34:59 +0530

    Added css loader and extract-text-webpack-plugin.

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button } from 'rmwc/Button';
+
 
 export default class Hello extends Component {
   render() {
     return (
       <div>
+        <Button>Easy</Button>
         Hello React!
     </div>
     )
